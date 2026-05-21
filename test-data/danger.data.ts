c6cd698f70849db2fdb297049a8bcc4154352e52
Hack <138391817+Phubadinhk@users.noreply.github.com>
2026-05-21 13:43:39 +0700

    Update send summary to Microsoft Teams and Test report

--- test-data/danger.data.ts
+++ test-data/danger.data.ts
@@ -3,7 +3,7 @@ export const DANGER_PERFORMANCE_DATA = {
   ROOT_URL: "https://ndwc-cap-kiosk-dev.azurewebsites.net",
   PATH_TEMPLATE: "/KioskClient/DangerSituation/DangerEvent/{provinceId}",
 
-  TOTAL_RUNS: 10,
+  TOTAL_RUNS: 1,
   TEST_TIMEOUT: 1800000,
   NAVIGATION_TIMEOUT: 120000,
   ROOT_URL_TIMEOUT: 60000,
@@ -11,8 +11,8 @@ export const DANGER_PERFORMANCE_DATA = {
 
   PROVINCE_IDS: [
     73, 7, 9, 12, 15,
-    18, 22, 27, 31, 35,
-    39, 42, 46, 50, 54,
-    58, 63, 67, 72, 77,
+    // 18, 22, 27, 31, 35,
+    // 39, 42, 46, 50, 54,
+    // 58, 63, 67, 72, 77,
   ],
 };
--- test-data/home.data.ts
+++ test-data/home.data.ts
@@ -2,22 +2,22 @@ export const HOME_PERFORMANCE_DATA = {
   BASE_URL: "https://dev-ndwc.azurewebsites.net",
   PATH_TEMPLATE: "/kioskclient/mainpage/{provinceId}/{stationId}",
 
-  TOTAL_RUNS: 10,
+  TOTAL_RUNS: 1,
   TEST_TIMEOUT: 1800000,
   NAVIGATION_TIMEOUT: 120000,
   WAIT_UNTIL: "networkidle" as const,
 
   PROVINCE_IDS: [
     3, 7, 9, 12, 15,
-    18, 22, 27, 31, 35,
-    39, 42, 46, 50, 54,
-    58, 63, 67, 72, 77,
+    // 18, 22, 27, 31, 35,
+    // 39, 42, 46, 50, 54,
+    // 58, 63, 67, 72, 77,
   ],
 
   STATION_IDS: [
     762, 712, 729, 742, 640,
-    659, 739, 658, 656, 657,
-    828, 854, 677, 688, 670,
-    820, 858, 640, 802, 643,
+    // 659, 739, 658, 656, 657,
+    // 828, 854, 677, 688, 670,
+    // 820, 858, 640, 802, 643,
   ],
 };
